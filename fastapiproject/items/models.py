e4fab4d2807cb431ac406fd86581c6fd19cd2cc6
Raziq Din <144256646+nidqija@users.noreply.github.com>
2025-12-04 04:18:17 +0800

    added admin_dashboard

--- fastapiproject/items/models.py
+++ fastapiproject/items/models.py
@@ -2,9 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+
+
+class Admin(models.Model):
+   admin_id = models.AutoField(primary_key=True , default=1)
+   admin_name = models.CharField(max_length=255)
+   admin_email = models.EmailField(unique=True)
+   admin_password = models.CharField(max_length=255)
+
+
+
+
 class Item(models.Model):
     item_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    item_name = models.CharField(max_length=100 , default="")
+    item_amount = models.IntegerField()
+    admin_id = models.ForeignKey(Admin , on_delete=models.CASCADE)
+    
 
     def __str__(self):
-     return self.name
+     return f"{self.item_name} ({self.item_amount})"
--- fastapiproject/items/urls.py
+++ fastapiproject/items/urls.py
@@ -2,7 +2,7 @@
 # from .views import index, custom_admin_login
 
 from django.urls import path
-from .views import index, custom_admin_login
+from .views import index, custom_admin_login , admin_dashboard
 
 urlpatterns = [
     # 1. Homepage: maps the root URL (e.g., http://127.0.0.1:8000/)
@@ -10,7 +10,9 @@ urlpatterns = [
     
     # 2. Login Page: maps to the custom authentication view
     # The trailing slash ('/') is the convention.
-    path('login/', custom_admin_login, name='login'), 
+    path('login/', custom_admin_login, name='login'),
+    path('admin_dashboard/' , admin_dashboard , name='admin_dashboard') 
+    
     
     # Optional: You can remove the conflicting and incorrect login path entries.
 ]
