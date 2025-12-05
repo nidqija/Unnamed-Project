c2737c9cbfd77efe379076155c1ee4465170749d
Raziq Din <144256646+nidqija@users.noreply.github.com>
2025-12-05 16:52:32 +0800

    added items count

--- fastapiproject/items/views.py
+++ fastapiproject/items/views.py
@@ -15,16 +15,28 @@ def admin_dashboard(request):
 
     admin = Admin.objects.get(admin_id=admin_id)
 
+    get_admin_items = Item.objects.filter(admin_id = admin)
+
+    get_admin_items_count = get_admin_items.count()
+    
+
+    
+
     if request.method == 'POST':
-        item_name = request.POST.get('item_name')
-        item_amount = request.POST.get('item_amount')
+      item_name = request.POST.get('item_name')
+      item_amount = request.POST.get('item_amount')
 
-        Item.objects.create(item_name=item_name, item_amount=item_amount, admin_id=admin)
+      Item.objects.create(item_name=item_name, item_amount=item_amount, admin_id=admin)
 
-        success_message = f"Item '{item_name}' added successfully!"
-        return render(request, 'admin_dashboard.html', {'success': success_message, 'admin': admin})
+    # Re-fetch items and count after creation
+      get_admin_items = Item.objects.filter(admin_id=admin)
+      get_admin_items_count = get_admin_items.count()
 
-    return render(request, 'admin_dashboard.html', {'admin': admin})
+      return redirect('admin_dashboard' )
+
+    
+
+    return render(request, 'admin_dashboard.html', {'admin': admin , 'get_admin_items_count' : get_admin_items_count , 'get_admin_items' : get_admin_items} )
 
 
 def insert_admin(request):
